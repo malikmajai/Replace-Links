37a488523655be92e89ce017235c46bbc8fb4e47
malikmajai <92770716+malikmajai@users.noreply.github.com>
2022-11-23 20:35:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 #LINKS
 EBAY = "?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338935868&customid=&toolid=10001&mkevt=1"
+EBAY_USERNAME = "?mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338935868&customid=&toolid=10001&mkevt=1"
 AMAZON = "/&linkCode=ll1&tag=rattlepokem09-20&linkId=a763430c7b3981df41aa0d051f6c38cd&language=enUS&ref=as_li_ss_tl"
 AMAZON_CA = "/&linkCode=ll1&tag=rattlepokem0d-20&linkId=f8e2a79acbd9833564c3701785a51b20&language=enCA&ref=as_li_ss_tl"
 
